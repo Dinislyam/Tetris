327030169587c918d89c6a0045909ba0eeae2e9a
Dinislyam <dentugushev2@gmail.com>
2020-11-29 19:13:01 +0400

    lesson5

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -8,11 +8,11 @@ namespace Tetris
         {
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
+            Square s = new Square(10, 5, '*');
+            s.Draw();
 
             Point p1 = new Point(5, 4, '*');
-            p1.Draw();
-
-            
+            p1.Draw();            
         }
         
     }
