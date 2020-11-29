087796cc65f0c3ba193c1650d3da7478c9761703
Dinislyam <dentugushev2@gmail.com>
2020-11-29 18:22:30 +0400

    Вывод точки на экран

---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            Draw(5, 5, 'c');
+        }
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}
